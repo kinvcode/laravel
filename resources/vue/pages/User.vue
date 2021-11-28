ca41a5ddb86da334a620b4663fe84450e86df8c0
wuyueyang <396981577@qq.com>
2021-11-28 15:36:41 +0800

    划分前后端分离路由

---
+++ resources/vue/pages/User.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hello">
+    <h1>用户</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'User',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  margin: 40px 0 0;
+}
+</style>
--- resources/vue/routers.js
+++ resources/vue/routers.js
@@ -1,18 +1,29 @@
 import Vue    from 'vue'
 import Router from 'vue-router'
+import Home from './pages/Home'; // 【重要】不可删除
 Vue.use(Router)
 
-import Home from './components/Home';
-
-var router = new Router({
+let router = new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: '',
-      component: Home
-      // component: resolve => require(['./components/Home'], resolve)
-      // component: ()=>import('./components/Home')
-    }
+      component: () => import('./pages/Home')
+    },
+    {
+      path: '/home/user',
+      name: 'user',
+      component: () => import('./pages/User')
+    },
+    {
+      path: '/home',
+      redirect: '/',
+    },
+    {
+      path: '/home/*',
+      redirect: '/',
+    },
   ]
 });
 export default router;
